c335957e9a7d43efc9272b8132ed49e781b9842c
OlesiaKubska <kublesia0908@gmail.com>
2023-05-01 21:34:04 +0200

    add js 02-timer.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,21 +8,21 @@ startBtn.addEventListener('click', onStartClick);
 stopBtn.addEventListener('click', onStopClick);
 
 function onStartClick() {
-startBtn.disabled = true; // Вимикаємо кнопку "Start"
-intervalId = setInterval(changeBodyBgColor, 1000); // Запускаємо зміну кольору фону щосекунди
+    startBtn.disabled = true; // Вимикаємо кнопку "Start"
+    intervalId = setInterval(changeBodyBgColor, 1000); // Запускаємо зміну кольору фону щосекунди
 }
 
 function onStopClick() {
-startBtn.disabled = false; // Вмикаємо кнопку "Start" знову
-clearInterval(intervalId); // Зупиняємо зміну кольору фону
+    startBtn.disabled = false; // Вмикаємо кнопку "Start" знову
+    clearInterval(intervalId); // Зупиняємо зміну кольору фону
 }
 
 function changeBodyBgColor() {
-const color = getRandomHexColor();
-body.style.backgroundColor = color; // Змінюємо колір фону
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color; // Змінюємо колір фону
 }
 
 //Функція getRandomHexColor використовується для генерації випадкового кольору у форматі шістнадцяткового коду
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
