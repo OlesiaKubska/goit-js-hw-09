6fc9a1da058695f792c2d5657ee001816047a1cc
OlesiaKubska <kublesia0908@gmail.com>
2023-05-02 20:43:30 +0200

    add js to the 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from "notiflix";
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit); //додали обробник події submit до форми, який викликає функцію handleSubmit
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  
+  const amount = parseInt(event.target.elements.amount.value);
+  const delay = parseInt(event.target.elements.delay.value);
+  const step = parseInt(event.target.elements.step.value);
+  const promise = []; //створюємо масив promises, в який додаємо стільки промісів, скільки введ
+
+  for (let i = 0; i < amount; i++) {
+    promise.push(createPromise(i, delay + i * step));
   }
+//Коли всі проміси будуть створені, використовуємо Promise.all для очікування їх виконання. 
+  Promise.all(promises)
+    .then((results) => {
+      results.forEach(({ position, delay }) => {
+        notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      });
+    })
+    .catch((results) => {
+      results.forEach(({ position, delay }) => {
+        notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    });
 }
